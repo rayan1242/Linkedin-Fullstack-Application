1a3bc6b5b79abaa78955458ea00955bc9991ce53
Aditya <shindeaditya0258@gmail.com>
2024-10-23 14:02:07 -0500

    Database Connection Implementation

---
+++ src/db.py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+def connect_to_database():
+    try:
+        connection = mysql.connector.connect(
+            host="127.0.0.1",
+            user="root",
+            password="Aditya.dev31",
+            database="linkedin"
+        )
+        print("\nConnected to the database successfully!!")
+        return connection
+    except mysql.connector.Error as err:
+        print(f"Error connecting to the database: {err}")
+        return None
