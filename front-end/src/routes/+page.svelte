d0275dd0574cf7e0e567043842f27acbcbbbf38a
rayan1242 <rayyanmaindargi12@gmail.com>
2024-11-13 19:59:45 -0600

    added components and other frontend functons

--- front-end/src/routes/+page.svelte
+++ front-end/src/routes/+page.svelte
@@ -1,59 +1,15 @@
-<script lang="ts">
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcomeFallback from '$lib/images/svelte-welcome.png';
-</script>
+<script>
+    import CreateUser from '../routes/CreateUser.svelte';
+    import UserList from '../routes/UserList.svelte';
+    import UpdateUser from '../routes/UpdateUser.svelte';
+    import DeleteUser from '../routes/DeleteUser.svelte';
+  </script>
+  
+  <main>
+    <h1>CRUD Application</h1>
+    <CreateUser />
+    <UserList />
+    <UpdateUser userId={1} /> <!-- Replace with dynamic user ID -->
+    <DeleteUser userId={1} /> <!-- Replace with dynamic user ID -->
+  </main>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcomeFallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
---
+++ front-end/src/routes/UserList.svelte
@@ -0,0 +1,36 @@
+<script>
+    import { onMount } from 'svelte';
+    import axios from 'axios';
+
+    /**
+     * @typedef {Object} User
+     * @property {number} user_id
+     * @property {string} name
+     * @property {string} dob
+     * @property {string} profile_pic
+     * @property {string} location_city
+     * @property {string} location_state
+     * @property {string} location_country
+     */
+
+    /** @type {User[]} */
+    let users = [];
+
+    onMount(async () => {
+        try {
+            const response = await axios.get('http://localhost:3000/users');
+            users = response.data.users;
+        } catch (error) {
+            console.error(error);
+        }
+    });
+</script>
+
+<div>
+    <h2>User List</h2>
+    <ul>
+        {#each users as user}
+            <li>{user.name}</li>
+        {/each}
+    </ul>
+</div>
